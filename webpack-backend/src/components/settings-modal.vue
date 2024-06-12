5bfb9c5c82ae5b4c2d83bc3907a49c56fc332495
Nahin Akbar <71415182+nahin-web@users.noreply.github.com>
2024-06-12 05:58:59 +0000

    WIP: designing the settings modal

--- webpack-backend/src/components/settings-modal.vue
+++ webpack-backend/src/components/settings-modal.vue
@@ -4,107 +4,161 @@
 			class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex"
 			id="modal-example-small"
 		>
-			<div class="relative w-auto my-6 mx-auto max-w-6xl">
-				<!--content-->
-				<div
-					class="border-0 rounded-lg shadow-lg relative flex flex-col p-12 pb-0 w-full bg-white outline-none focus:outline-none flex-auto"
-					style="height: 75vh; width: 100vh"
-				>
-					<!--header-->
-					<div
-						class="flex items-start justify-between border-b border-solid border-gray-200 rounded-t"
-					>
-						<h3 class="text-3xl font-semibold">Table Settings</h3>
-					</div>
-					<!--body-->
-					<div class="shadow-lg p-3 rounded-lg shadow-gray-500">
-						<h5 v-if="!isLoading" class="text-base my-2 font-semibold">
-							Header Fonts
-						</h5>
-						<div v-if="!isLoading" class="relative grid grid-cols-3 flex pt-0">
-							<fonts-select
-								:options="getFontFamilyList(this)"
-								fontZone="tableHeader"
-								context="fontFamily"
-								@updateFont="handleFontUpdate"
-							/>
-							<fonts-select
-								:options="getSizes(this)"
-								font-zone="tableHeader"
-								context="fontSize"
-							/>
-							<fonts-select
-								:options="findFontFamilyWeights(this)"
-								font-zone="tableHeader"
-								:key="componentKey"
-								ref="weightDropDown"
-								context="fontWeight"
-							/>
+			<!--content-->
+			<div class="mx-4 w-full rounded-xl bg-gray-100 p-4 md:w-1/2 lg:w-1/3">
+				<!--MODAL HEADER-->
+				<div class="items center flex justify-between border-b border-gray-200">
+					<div class="flex items-center justify-center">
+						<div class="bg-white">
+							<nav class="flex flex-col sm:flex-row">
+								<button
+									class="block border-b-2 border-blue-500 px-6 py-4 font-medium text-blue-500 text-gray-600 hover:text-blue-500 focus:outline-none"
+								>
+									Tab 1</button
+								><button
+									class="block px-6 py-4 text-gray-600 hover:text-blue-500 focus:outline-none"
+								>
+									Tab 2</button
+								><button
+									class="block px-6 py-4 text-gray-600 hover:text-blue-500 focus:outline-none"
+								>
+									Tab 3</button
+								><button
+									class="block px-6 py-4 text-gray-600 hover:text-blue-500 focus:outline-none"
+								>
+									Tab 4
+								</button>
+							</nav>
 						</div>
 					</div>
-					<div class="shadow-lg mt-2 p-3 rounded-lg shadow-gray-500">
-						<h5 v-if="!isLoading" class="text-base my-2 font-semibold">
-							Table Cell Fonts
-						</h5>
-						<div v-if="!isLoading" class="relative grid grid-cols-3 flex pt-0">
-							<fonts-select
-								:options="getFontFamilyList(this)"
-								fontZone="tableBodyHeader"
-								@updateFont="handleFontUpdate"
-								context="fontFamily"
-							/>
-							<fonts-select
-								:options="getSizes(this)"
-								fontZone="tableBodyHeader"
-								context="fontSize"
-							/>
-							<fonts-select
-								:options="findFontFamilyWeights(this)"
-								fontZone="tableBodyHeader"
-								:key="componentKey"
-								context="fontWeight"
-								ref="weightDropDown"
-							/>
-						</div>
-					</div>
-					<label
-						v-for="obj in Object.keys(settingsItems)"
-						:key="obj"
-						v-bind:style="{ display: evaluateDeps(obj) }"
-						class="inline-flex mt-2 items-center"
-					>
-						<input
-							type="checkbox"
-							class="form-checkbox"
-							v-if="settingsItems[obj].type == 'boolean'"
-							:checked="settingsItemProps[obj]"
-							@change="handleChkbx($event, obj, settingsItemProps, $store)"
-						/>
-						<toolcool-color-picker
-							:key="obj"
-							@change="
-								handleColorChange($event, obj, settingsItemProps, $store)
-							"
-							v-if="settingsItems[obj].type == 'color'"
-							:color="settingsItemProps[obj]"
-						/>
-						<span class="ml-2 font-semibold">{{
-							settingsItems[obj].title
-						}}</span>
-					</label>
-					<p v-if="isLoading" class="font-semibold text-center text-gray">
-						Please Wait
-					</p>
-					<!--footer-->
 					<div
-						class="flex items-center justify-end p-6 border-t border-solid border-gray-200 rounded-b"
+						class="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-gray-300 font-sans text-gray-500 hover:bg-gray-500 hover:text-gray-300"
 					>
-						<button
-							class="text-purple-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-							type="button"
-							@click="handleModalClose()"
+						x
+					</div>
+				</div>
+
+				<!--MODAL BODY-->
+				<div class="my-4">
+					<p class="text-sm">Share this link via</p>
+
+					<div class="my-4 flex justify-around">
+						<!--FACEBOOK ICON-->
+						<div
+							class="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border border-blue-200 fill-[#1877f2] shadow-xl hover:bg-[#1877f2] hover:fill-white hover:shadow-blue-500/50"
 						>
-							Close
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+							>
+								<path
+									d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"
+								></path>
+							</svg>
+						</div>
+						<!--TWITTER ICON-->
+						<div
+							class="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border border-blue-200 fill-[#1d9bf0] shadow-xl hover:bg-[#1d9bf0] hover:fill-white hover:shadow-sky-500/50"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+							>
+								<path
+									d="M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809.324.037.636.05.973.05a8.07 8.07 0 0 0 5.001-1.721 4.036 4.036 0 0 1-3.767-2.793c.249.037.499.062.761.062.361 0 .724-.05 1.061-.137a4.027 4.027 0 0 1-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 0 1-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 0 0 8.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 0 1 4.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 0 0 2.556-.973 4.02 4.02 0 0 1-1.771 2.22 8.073 8.073 0 0 0 2.319-.624 8.645 8.645 0 0 1-2.019 2.083z"
+								></path>
+							</svg>
+						</div>
+						<!--INSTAGRAM ICON-->
+						<div
+							class="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border border-pink-200 fill-[#bc2a8d] shadow-xl hover:bg-[#bc2a8d] hover:fill-white hover:shadow-pink-500/50"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+							>
+								<path
+									d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248 4.623 4.623 0 0 0 0-9.248zm0 7.627a3.004 3.004 0 1 1 0-6.008 3.004 3.004 0 0 1 0 6.008z"
+								></path>
+								<circle cx="16.806" cy="7.207" r="1.078"></circle>
+								<path
+									d="M20.533 6.111A4.605 4.605 0 0 0 17.9 3.479a6.606 6.606 0 0 0-2.186-.42c-.963-.042-1.268-.054-3.71-.054s-2.755 0-3.71.054a6.554 6.554 0 0 0-2.184.42 4.6 4.6 0 0 0-2.633 2.632 6.585 6.585 0 0 0-.419 2.186c-.043.962-.056 1.267-.056 3.71 0 2.442 0 2.753.056 3.71.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632 6.584 6.584 0 0 0 2.185.45c.963.042 1.268.055 3.71.055s2.755 0 3.71-.055a6.615 6.615 0 0 0 2.186-.419 4.613 4.613 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.186.043-.962.056-1.267.056-3.71s0-2.753-.056-3.71a6.581 6.581 0 0 0-.421-2.217zm-1.218 9.532a5.043 5.043 0 0 1-.311 1.688 2.987 2.987 0 0 1-1.712 1.711 4.985 4.985 0 0 1-1.67.311c-.95.044-1.218.055-3.654.055-2.438 0-2.687 0-3.655-.055a4.96 4.96 0 0 1-1.669-.311 2.985 2.985 0 0 1-1.719-1.711 5.08 5.08 0 0 1-.311-1.669c-.043-.95-.053-1.218-.053-3.654 0-2.437 0-2.686.053-3.655a5.038 5.038 0 0 1 .311-1.687c.305-.789.93-1.41 1.719-1.712a5.01 5.01 0 0 1 1.669-.311c.951-.043 1.218-.055 3.655-.055s2.687 0 3.654.055a4.96 4.96 0 0 1 1.67.311 2.991 2.991 0 0 1 1.712 1.712 5.08 5.08 0 0 1 .311 1.669c.043.951.054 1.218.054 3.655 0 2.436 0 2.698-.043 3.654h-.011z"
+								></path>
+							</svg>
+						</div>
+
+						<!--WHATSAPP ICON-->
+						<div
+							class="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border border-green-200 fill-[#25D366] shadow-xl hover:bg-[#25D366] hover:fill-white hover:shadow-green-500/50"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+							>
+								<path
+									fill-rule="evenodd"
+									clip-rule="evenodd"
+									d="M18.403 5.633A8.919 8.919 0 0 0 12.053 3c-4.948 0-8.976 4.027-8.978 8.977 0 1.582.413 3.126 1.198 4.488L3 21.116l4.759-1.249a8.981 8.981 0 0 0 4.29 1.093h.004c4.947 0 8.975-4.027 8.977-8.977a8.926 8.926 0 0 0-2.627-6.35m-6.35 13.812h-.003a7.446 7.446 0 0 1-3.798-1.041l-.272-.162-2.824.741.753-2.753-.177-.282a7.448 7.448 0 0 1-1.141-3.971c.002-4.114 3.349-7.461 7.465-7.461a7.413 7.413 0 0 1 5.275 2.188 7.42 7.42 0 0 1 2.183 5.279c-.002 4.114-3.349 7.462-7.461 7.462m4.093-5.589c-.225-.113-1.327-.655-1.533-.73-.205-.075-.354-.112-.504.112s-.58.729-.711.879-.262.168-.486.056-.947-.349-1.804-1.113c-.667-.595-1.117-1.329-1.248-1.554s-.014-.346.099-.458c.101-.1.224-.262.336-.393.112-.131.149-.224.224-.374s.038-.281-.019-.393c-.056-.113-.505-1.217-.692-1.666-.181-.435-.366-.377-.504-.383a9.65 9.65 0 0 0-.429-.008.826.826 0 0 0-.599.28c-.206.225-.785.767-.785 1.871s.804 2.171.916 2.321c.112.15 1.582 2.415 3.832 3.387.536.231.954.369 1.279.473.537.171 1.026.146 1.413.089.431-.064 1.327-.542 1.514-1.066.187-.524.187-.973.131-1.067-.056-.094-.207-.151-.43-.263"
+								></path>
+							</svg>
+						</div>
+
+						<!--TELEGRAM ICON-->
+						<div
+							class="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border border-sky-200 fill-[#229ED9] shadow-xl hover:bg-[#229ED9] hover:fill-white hover:shadow-sky-500/50"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+							>
+								<path
+									d="m20.665 3.717-17.73 6.837c-1.21.486-1.203 1.161-.222 1.462l4.552 1.42 10.532-6.645c.498-.303.953-.14.579.192l-8.533 7.701h-.002l.002.001-.314 4.692c.46 0 .663-.211.921-.46l2.211-2.15 4.599 3.397c.848.467 1.457.227 1.668-.785l3.019-14.228c.309-1.239-.473-1.8-1.282-1.434z"
+								></path>
+							</svg>
+						</div>
+					</div>
+
+					<p class="text-sm">Or copy link</p>
+					<!--BOX LINK-->
+					<div
+						class="mt-4 flex items-center justify-between border-2 border-gray-200 py-2"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							class="ml-2 fill-gray-500"
+						>
+							<path
+								d="M8.465 11.293c1.133-1.133 3.109-1.133 4.242 0l.707.707 1.414-1.414-.707-.707c-.943-.944-2.199-1.465-3.535-1.465s-2.592.521-3.535 1.465L4.929 12a5.008 5.008 0 0 0 0 7.071 4.983 4.983 0 0 0 3.535 1.462A4.982 4.982 0 0 0 12 19.071l.707-.707-1.414-1.414-.707.707a3.007 3.007 0 0 1-4.243 0 3.005 3.005 0 0 1 0-4.243l2.122-2.121z"
+							></path>
+							<path
+								d="m12 4.929-.707.707 1.414 1.414.707-.707a3.007 3.007 0 0 1 4.243 0 3.005 3.005 0 0 1 0 4.243l-2.122 2.121c-1.133 1.133-3.109 1.133-4.242 0L10.586 12l-1.414 1.414.707.707c.943.944 2.199 1.465 3.535 1.465s2.592-.521 3.535-1.465L19.071 12a5.008 5.008 0 0 0 0-7.071 5.006 5.006 0 0 0-7.071 0z"
+							></path>
+						</svg>
+
+						<input
+							class="w-full bg-transparent outline-none"
+							type="text"
+							placeholder="link"
+							value="https://boxicons.com/?query=link"
+						/>
+
+						<button
+							class="mr-2 rounded bg-indigo-500 px-5 py-2 text-sm text-white hover:bg-indigo-600"
+						>
+							Copy
 						</button>
 					</div>
 				</div>
